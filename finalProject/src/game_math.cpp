9ff530162e72c3a8d459678797969d41f539846f
riya jain <rjain35@illinois.edu>
2019-04-25 21:21:51 -0500

    Revert "made method hasLost more effficent by merging if statements and updated ints to size_t"
    This reverts commit 0f76bf03a3b484c78687e66666dab3fe7493f01f.

--- finalProject/src/game_math.cpp
+++ finalProject/src/game_math.cpp
@@ -6,19 +6,19 @@
 // One tile has the value two, the other one has the value two or four. 
 void Board::InitBoard() {
 	// Select a random position for the first tile.
-	size_t first_tile_postion = rand() % kNumOfTiles + 1;
-	size_t second_tile_position = first_tile_postion;
+	int first_tile_postion = rand() % kNumOfTiles + 1;
+	int second_tile_position = first_tile_postion;
 
 	// Select a random position for the second tile and ensure that it is not the same as the first.
 	while (second_tile_position == first_tile_postion) {
 		second_tile_position = rand() % kNumOfTiles + 1;
 	}
 	// Randomly select the value of the second tile to be two or four.
-	size_t second_tile_value = ChooseTwoOrFour();
+	int second_tile_value = ChooseTwoOrFour();
 	
-	size_t board_position = 1;
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	int board_position = 1;
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			// Traverse through the board and initialize each value to be 0 unless it is the first or second tile.
 			if (board_position == first_tile_postion) {
 				board_[i][j].value = kTwo;
@@ -37,8 +37,8 @@ void Board::InitBoard() {
 
 // Simply print the board for testing purposes.
 void Board::RudimentaryPrint() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			std::cout << board_[i][j].value << " ";
 		}
 		std::cout << endl;
@@ -48,8 +48,8 @@ void Board::RudimentaryPrint() {
 // Return a vector of positions in the board where the value is 0 (essentially, that position is 'empty').
 vector<size_t> Board::FindEmptyPositions() {
 	vector<size_t> possible_postions;
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			if (board_[i][j].value == 0) {
 				possible_postions.push_back(board_[i][j].board_position);
 			}
@@ -60,8 +60,8 @@ vector<size_t> Board::FindEmptyPositions() {
 
 // Traverse through the value of each tile in the board to see if 2048 has been reached yet.
 bool Board::hasWon() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			if (board_[i][j].value == winning_number) {
 				return true;
 			}
@@ -72,10 +72,10 @@ bool Board::hasWon() {
 
 // Move all the tiles leftwards so that no zeroes lie between nonzero values.
 void Board::CompressLeft() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			if (board_[i][j].value == 0) {
-				for (size_t k = j + 1; k < kBoardDimension; k++) {
+				for (int k = j + 1; k < kBoardDimension; k++) {
 					// if a tile value is zero, and the tile to the right is not zero, change the first tile's value to that of the second tile, and the second tile's value to 0.
 					if (board_[i][k].value != 0) {
 						board_[i][j].value = board_[i][k].value;
@@ -90,8 +90,8 @@ void Board::CompressLeft() {
 
 // Merges values of tiles leftwards.
 void Board::MergeLeft() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension - 1; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension - 1; j++) {
 			// If the tile value doesn't equal zero and the tile to the right of it has the value zero, the value of the first tile equals their sum and the value of the second tile becomes zero.
 			if (board_[i][j].value != 0 && board_[i][j].value == board_[i][j + 1].value) {
 				board_[i][j].value += board_[i][j + 1].value;
@@ -107,11 +107,11 @@ void Board::MergeLeft() {
 
 // Move all the tiles upwards so that no zeroes lie between nonzero values.
 void Board::CompressUp() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			// If a tile value is zero, and the tile below it is not zero, change the first tile's value to that of the second tile, and the second tile's value to 0.
 			if (board_[j][i].value == 0) {
-				for (size_t k = j + 1; k < kBoardDimension; k++) {
+				for (int k = j + 1; k < kBoardDimension; k++) {
 					if (board_[k][i].value != 0) {
 						board_[j][i].value = board_[k][i].value;
 						board_[k][i].value = 0;
@@ -125,8 +125,8 @@ void Board::CompressUp() {
 
 // Merges values of tiles upwards.
 void Board::MergeUp() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension - 1; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension - 1; j++) {
 			// If the tile value doesn't equal zero and the tile below it has the value zero, the value of the first tile equals their sum and the value of the second tile becomes zero.
 			if (board_[j][i].value != 0 && board_[j][i].value == board_[j + 1][i].value) {
 				board_[j][i].value += board_[j + 1][i].value;
@@ -142,11 +142,11 @@ void Board::MergeUp() {
 
 // Move all the tiles rightwards so that no zeroes lie between nonzero values.
 void Board::CompressRight() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = kBoardDimension - 1; j >= 0; j--) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = kBoardDimension - 1; j >= 0; j--) {
 			// If a tile value is zero, and the tile to the left is not zero, change the first tile's value to that of the second tile, and the second tile's value to 0.
 			if (board_[i][j].value == 0) {
-				for (size_t k = j - 1; k >= 0; k--) {
+				for (int k = j - 1; k >= 0; k--) {
 					if (board_[i][k].value != 0) {
 						board_[i][j].value = board_[i][k].value;
 						board_[i][k].value = 0;
@@ -160,8 +160,8 @@ void Board::CompressRight() {
 
 // Merges values of tiles rightwards.
 void Board::MergeRight() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = kBoardDimension - 1; j > 0; j--) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = kBoardDimension - 1; j > 0; j--) {
 			// If the tile value doesn't equal zero and the tile to the left of it has the value zero, the value of the first tile equals their sum and the value of the second tile becomes zero.
 			if (board_[i][j].value != 0 && board_[i][j].value == board_[i][j - 1].value) {
 				board_[i][j].value += board_[i][j - 1].value;
@@ -177,11 +177,11 @@ void Board::MergeRight() {
 
 // Move all the tiles downwards so that no zeroes lie between nonzero values.
 void Board::CompressDown() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = kBoardDimension - 1; j > 0; j--) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = kBoardDimension - 1; j >= 0; j--) {
 			if (board_[j][i].value == 0) {
 				// If a tile value is zero, and the tile above it is not zero, change the first tile's value to that of the second tile, and the second tile's value to 0.
-				for (size_t k = j - 1; k >= 0; k--) {
+				for (int k = j - 1; k >= 0; k--) {
 					if (board_[k][i].value != 0) {
 						board_[j][i].value = board_[k][i].value;
 						board_[k][i].value = 0;
@@ -195,8 +195,8 @@ void Board::CompressDown() {
 
 // Merges values of tiles downwards.
 void Board::MergeDown() {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = kBoardDimension - 1; j > 0; j--) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = kBoardDimension - 1; j > 0; j--) {
 			// If the tile value doesn't equal zero and the tile above it has the value zero, the value of the first tile equals their sum and the value of the second tile becomes zero.
 			if (board_[j][i].value != 0 && board_[j][i].value == board_[j - 1][i].value) {
 				board_[j][i].value += board_[j - 1][i].value;
@@ -230,9 +230,9 @@ void Board::LoopThroughGame() {
 }
 
 void Board::InitCopy() {
-	size_t counter = 1;
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	int counter = 1;
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			board_copy_[i][j].board_position = counter;
 			board_copy_[i][j].value = 0;
 			counter++;
@@ -241,16 +241,16 @@ void Board::InitCopy() {
 }
 
 void Board::CopyBoard(Tile initial[4][4], Tile copy[4][4]) {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			copy[i][j].value = initial[i][j].value;
 		}
 	}
 }
 
 bool Board::BoardsAreEqual(Tile initial[4][4], Tile copy[4][4]) {
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			if (copy[i][j].value != initial[i][j].value) {
 				return false;
 			}
@@ -263,9 +263,13 @@ bool Board::hasLost() {
 	if (FindEmptyPositions().size() != 0) {
 		return false;
 	}
-	for (size_t i = 0; i < kBoardDimension - 1; i++) {
-		for (size_t j = 0; j < kBoardDimension - 1; j++) {
-			if (board_[i][j].value == board_[i][j + 1].value || board_[i][j].value == board_[i + 1][j].value) {
+	for (int i = 0; i < kBoardDimension - 1; i++) {
+		for (int j = 0; j < kBoardDimension - 1; j++) {
+			// combine these into one if
+			if (board_[i][j].value == board_[i][j + 1].value) {
+				return false;
+			}
+			if (board_[i][j].value == board_[i + 1][j].value) {
 				return false;
 			}
 		}
@@ -292,8 +296,8 @@ void Board::MakeMoves(char input) {
 	}
 }
 
-size_t Board::ChooseTwoOrFour() {
-	size_t random = rand() % 2;
+int Board::ChooseTwoOrFour() {
+	int random = rand() % 2;
 	if (random == 0) {
 		return kTwo;
 	} else {
@@ -302,9 +306,9 @@ size_t Board::ChooseTwoOrFour() {
 }
 
 void Board::SpawnNewTwo(vector<size_t> possible_positions) {
-	size_t new_two_position = possible_positions[rand() % possible_positions.size()];
-	for (size_t i = 0; i < kBoardDimension; i++) {
-		for (size_t j = 0; j < kBoardDimension; j++) {
+	int new_two_position = possible_positions[rand() % possible_positions.size()];
+	for (int i = 0; i < kBoardDimension; i++) {
+		for (int j = 0; j < kBoardDimension; j++) {
 			if (board_[i][j].board_position == new_two_position) {
 				board_[i][j].value = kTwo;
 				break;
