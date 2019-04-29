421fe294c18bd5be032fa62d63dc8e73751285de
riya jain <rjain35@illinois.edu>
2019-04-29 01:17:05 -0500

    renamed variables and moved to private, cleaned up header

--- finalProject/src/board.h
+++ finalProject/src/board.h
@@ -8,10 +8,50 @@ class Board {
 	struct Tile {
 		size_t value;
 		size_t board_position;
-		vector<size_t> contributors;
+		//vector<size_t> contributors;
 	};
 
 public:
+	void InitBoard();
+
+	size_t ChooseTwoOrFour();
+
+	void SpawnNewTwo(vector<size_t> possible_positions);
+
+	void RudimentaryPrint();
+
+	vector<size_t> FindEmptyPositions();
+	
+	void InitCopy();
+
+	void CopyBoard(Tile initial[4][4], Tile copy[4][4]);
+
+	bool BoardsAreEqual(Tile initial[4][4], Tile copy[4][4]);
+
+	bool HasLost();
+
+	void WinningNumberReached(size_t value);
+
+	void SetUpGame();
+	//void ClearContributors();
+
+	void CompressLeft();
+
+	void MergeLeft();
+
+	void CompressUp();
+
+	void MergeUp();
+
+	void CompressRight();
+
+	void MergeRight();
+
+	void CompressDown();
+
+	void MergeDown();
+
+private:
 	Tile board_[4][4];
 	Tile board_copy_[4][4];
 
@@ -20,30 +60,7 @@ public:
 
 	const size_t kBoardDimension = 4;
 	const size_t kNumOfTiles = 16;
-	const size_t kTwo = 2;
-	const size_t kFour = 4;
+	const size_t kStartingNumber1 = 2;
+	const size_t kStartingNumber2 = 4;
 	const size_t kWinningNumber = 2048;
-
-	void InitBoard();
-	size_t ChooseTwoOrFour();
-	void SpawnNewTwo(vector<size_t> possible_positions);
-	void RudimentaryPrint();
-	vector<size_t> FindEmptyPositions();
-	
-	void InitCopy();
-	void CopyBoard(Tile initial[4][4], Tile copy[4][4]);
-	bool BoardsAreEqual(Tile initial[4][4], Tile copy[4][4]);
-	bool HasLost();
-	void WinningNumberReached(size_t value);
-	void SetUpGame();
-	void ClearContributors();
-
-	void CompressLeft();
-	void MergeLeft();
-	void CompressUp();
-	void MergeUp();
-	void CompressRight();
-	void MergeRight();
-	void CompressDown();
-	void MergeDown();
 };
