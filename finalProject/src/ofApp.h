873c9823332fd6bd29504348d0cad526a762c7c6
riya jain <rjain35@illinois.edu>
2019-04-29 21:11:54 -0500

    moved items to private

--- finalProject/src/ofApp.h
+++ finalProject/src/ofApp.h
@@ -5,9 +5,20 @@
 
 class ofApp : public ofBaseApp {
 
-	ofTrueTypeFont trench_font;
+private:
+
+	enum GameState {
+		BEGIN,
+		SETUP,
+		PLAY,
+		WIN,
+		LOSS
+	};
+
+	ofTrueTypeFont pixel_font;
 	ofSoundPlayer winning_tune;
 	ofSoundPlayer click;
+
 	ofImage tile_two;
 	ofImage tile_four;
 	ofImage tile_eight;
@@ -19,58 +30,70 @@ class ofApp : public ofBaseApp {
 	ofImage tile_five_twelve;
 	ofImage tile_ten_twenty_four;
 	ofImage tile_twenty_forty_eight;
+		
+	void setup();
 
-	private:
-		enum GameState {
-			BEGIN,
-			SETUP,
-			PLAY,
-			WIN,
-			LOSS
-		};
+	void update();
 
-	public:
-		void setup();
-		void update();
-		void draw();
-		void drawBoard();
-		void drawScore();
-		void drawWin();
-		void drawLoss();
-		void drawBeginningStage();
-		void drawTiles();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		void CheckGameOver();
-		void SetUpTileMap();
-		void Slide();
+	void drawBoard();
 
-		const int kTileDimension = 100;
-		const int kBackBoardDimension = 450;
-		int spacing = 10;
-		bool should_update = true;
-		bool should_move_board = false;
-		GameState current_state = BEGIN;
+	void drawScore();
 
-		const std::string kBeginGame = "Press 'B' to begin 2048";
-		const std::string kScore = "Score: ";
-		const std::string kWonMessage = "You WON!";
-		const std::string kLostMessage = "You lost";
+	void drawWin();
 
-		map<int, ofImage> tiles;
-		map<int, ofImage>::iterator tile_to_draw;
-		map<int, pair<float, float>> positions;
-		float x = (ofGetWindowWidth() / 2) - (2 * kTileDimension) - spacing;
-		float y = (ofGetWindowHeight() / 2) - (2 * kTileDimension) - spacing;
+	void drawLoss();
+
+	void drawBeginningStage();
+
+	void drawTiles();
+
+	void keyPressed(int key);
+
+	void keyReleased(int key);
+
+	void mouseMoved(int x, int y );
+
+	void mouseDragged(int x, int y, int button);
+
+	void mousePressed(int x, int y, int button);
+
+	void mouseReleased(int x, int y, int button);
+	
+	void mouseEntered(int x, int y);
+	
+	void mouseExited(int x, int y);
+	
+	void windowResized(int w, int h);
+	
+	void dragEvent(ofDragInfo dragInfo);
+	
+	void gotMessage(ofMessage msg);
+	
+	void CheckGameOver();
+	
+	void SetUpTileMap();
+	
+	void Slide();
+
+	const int kTileDimension = 100;
+	const int kBackBoardDimension = 450;
+	const int kSpacing = 10;
+
+	bool should_update = true;
+	bool should_move_board = false;
+	GameState current_state = BEGIN;
+
+	const std::string kBeginGame = "Press 'B' to begin 2048";
+	const std::string kScore = "Score: ";
+	const std::string kWonMessage = "You WON!";
+	const std::string kLostMessage = "You lost";
+
+	map<int, ofImage> tiles;
+	map<int, ofImage>::iterator tile_to_draw;
+	map<int, pair<float, float>> positions;
+	float x = (ofGetWindowWidth() / 2) - (2 * kTileDimension) - kSpacing;
+	float y = (ofGetWindowHeight() / 2) - (2 * kTileDimension) - kSpacing;
 
 };
