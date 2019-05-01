b7c8a6a495c9daed35dd696611e2e4d9babb21d1
riya jain <rjain35@illinois.edu>
2019-05-01 00:54:26 -0500

    added test cases

--- finalProject/src/test.cpp
+++ finalProject/src/test.cpp
@@ -1,4 +1,4 @@
-/*  #pragma once
+/*#pragma once
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp";
@@ -44,5 +44,124 @@ TEST_CASE("Check if CompressLeft and MergeLeft operate correctly") {
 	REQUIRE (tester.board_[3][1].value == 8);
 	REQUIRE (tester.board_[3][2].value == 2);
 	REQUIRE (tester.board_[3][3].value == 0);
+}
 
-}*/
+TEST_CASE("Check if CompressRight and MergeRight operate correctly") {
+	tester.InitBoard();
+	tester.board_[0][0].value = 32;
+	tester.board_[0][1].value = 8;
+	tester.board_[0][2].value = 0;
+	tester.board_[0][3].value = 0;
+	tester.board_[1][0].value = 8;
+	tester.board_[1][1].value = 4;
+	tester.board_[1][2].value = 0;
+	tester.board_[1][3].value = 0;
+	tester.board_[2][0].value = 2;
+	tester.board_[2][1].value = 0;
+	tester.board_[2][2].value = 0;
+	tester.board_[2][3].value = 2;
+	tester.board_[3][0].value = 0;
+	tester.board_[3][1].value = 0;
+	tester.board_[3][2].value = 0;
+	tester.board_[3][3].value = 0;
+
+	tester.CompressRight();
+	tester.MergeRight();
+
+	REQUIRE(tester.board_[0][0].value == 0);
+	REQUIRE(tester.board_[0][1].value == 0);
+	REQUIRE(tester.board_[0][2].value == 32);
+	REQUIRE(tester.board_[0][3].value == 8);
+	REQUIRE(tester.board_[1][0].value == 0);
+	REQUIRE(tester.board_[1][1].value == 0);
+	REQUIRE(tester.board_[1][2].value == 8);
+	REQUIRE(tester.board_[1][3].value == 4);
+	REQUIRE(tester.board_[2][0].value == 0);
+	REQUIRE(tester.board_[2][1].value == 0);
+	REQUIRE(tester.board_[2][2].value == 0);
+	REQUIRE(tester.board_[2][3].value == 4);
+	REQUIRE(tester.board_[3][0].value == 0);
+	REQUIRE(tester.board_[3][1].value == 0);
+	REQUIRE(tester.board_[3][2].value == 0);
+	REQUIRE(tester.board_[3][3].value == 0);
+}
+
+TEST_CASE("Check if CompressUp and MergeUp operate correctly") {
+	tester.InitBoard();
+	tester.board_[0][0].value = 0;
+	tester.board_[0][1].value = 0;
+	tester.board_[0][2].value = 0;
+	tester.board_[0][3].value = 0;
+	tester.board_[1][0].value = 0;
+	tester.board_[1][1].value = 0;
+	tester.board_[1][2].value = 2;
+	tester.board_[1][3].value = 0;
+	tester.board_[2][0].value = 8;
+	tester.board_[2][1].value = 4;
+	tester.board_[2][2].value = 0;
+	tester.board_[2][3].value = 0;
+	tester.board_[3][0].value = 16;
+	tester.board_[3][1].value = 8;
+	tester.board_[3][2].value = 4;
+	tester.board_[3][3].value = 2;
+
+	tester.CompressUp();
+	tester.MergeUp();
+
+	REQUIRE(tester.board_[0][0].value == 8);
+	REQUIRE(tester.board_[0][1].value == 4);
+	REQUIRE(tester.board_[0][2].value == 2);
+	REQUIRE(tester.board_[0][3].value == 2);
+	REQUIRE(tester.board_[1][0].value == 16);
+	REQUIRE(tester.board_[1][1].value == 8);
+	REQUIRE(tester.board_[1][2].value == 4);
+	REQUIRE(tester.board_[1][3].value == 0);
+	REQUIRE(tester.board_[2][0].value == 0);
+	REQUIRE(tester.board_[2][1].value == 0);
+	REQUIRE(tester.board_[2][2].value == 0);
+	REQUIRE(tester.board_[2][3].value == 0);
+	REQUIRE(tester.board_[3][0].value == 0);
+	REQUIRE(tester.board_[3][1].value == 0);
+	REQUIRE(tester.board_[3][2].value == 0);
+	REQUIRE(tester.board_[3][3].value == 0);
+}
+
+TEST_CASE("Check if CompressDown and MergeDown operate correctly") {
+	tester.InitBoard();
+	tester.board_[0][0].value = 32;
+	tester.board_[0][1].value = 8;
+	tester.board_[0][2].value = 4;
+	tester.board_[0][3].value = 0;
+	tester.board_[1][0].value = 4;
+	tester.board_[1][1].value = 0;
+	tester.board_[1][2].value = 0;
+	tester.board_[1][3].value = 0;
+	tester.board_[2][0].value = 0;
+	tester.board_[2][1].value = 0;
+	tester.board_[2][2].value = 0;
+	tester.board_[2][3].value = 0;
+	tester.board_[3][0].value = 0;
+	tester.board_[3][1].value = 0;
+	tester.board_[3][2].value = 2;
+	tester.board_[3][3].value = 0;
+
+	tester.CompressDown();
+	tester.MergeDown();
+
+	REQUIRE(tester.board_[0][0].value == 0);
+	REQUIRE(tester.board_[0][1].value == 0);
+	REQUIRE(tester.board_[0][2].value == 0);
+	REQUIRE(tester.board_[0][3].value == 0);
+	REQUIRE(tester.board_[1][0].value == 0);
+	REQUIRE(tester.board_[1][1].value == 0);
+	REQUIRE(tester.board_[1][2].value == 0);
+	REQUIRE(tester.board_[1][3].value == 0);
+	REQUIRE(tester.board_[2][0].value == 32);
+	REQUIRE(tester.board_[2][1].value == 0);
+	REQUIRE(tester.board_[2][2].value == 4);
+	REQUIRE(tester.board_[2][3].value == 0);
+	REQUIRE(tester.board_[3][0].value == 4);
+	REQUIRE(tester.board_[3][1].value == 8);
+	REQUIRE(tester.board_[3][2].value == 2);
+	REQUIRE(tester.board_[3][3].value == 0);
+} */
